f819dffbbcdc59ea159a6d34568b2c578c97f545
Parnab Saha <1971147@tcs.com>
2023-11-08 15:54:24 +0530

    IDS eQ Approve Staff Reconciliation

---
+++ IDS eQ Approve Staff Reconciliation/Test1/src/DataBaseUtility.java
@@ -0,0 +1,61 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+import oracle.jdbc.pool.OracleDataSource;
+
+public class DataBaseUtility {
+	
+
+	public Connection getConnection(Map<String, String> dbParams) {
+		Connection con = null;
+		try {
+			OracleDataSource oracleDS = new OracleDataSource();
+			/*oracleDS.setURL("jdbc:oracle:thin:@eqsit-soa:16013:SOASIT");
+			oracleDS.setUser("eqfiles");
+			oracleDS.setPassword("eqfiles");*/
+			oracleDS.setURL(dbParams.get("HOST"));
+			oracleDS.setUser(dbParams.get("USER"));
+			oracleDS.setPassword(dbParams.get("PASSWORD"));
+
+			con = oracleDS.getConnection();
+		}
+		catch (Exception exp) {
+			exp.printStackTrace();
+		}
+	
+		return con;
+	}
+	
+	public void closeConnection(Connection con) {
+		try {
+			if(null != con)
+				con.close();
+		}
+		catch(Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+	
+	public void closePreparedStatement(PreparedStatement ps) {
+		try {
+			if(null != ps)
+				ps.close();
+		}
+		catch(Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+	
+	public void closeResultSet(ResultSet rs) {
+		try {
+			if(null != rs)
+				rs.close();
+		}
+		catch(Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+
+}
