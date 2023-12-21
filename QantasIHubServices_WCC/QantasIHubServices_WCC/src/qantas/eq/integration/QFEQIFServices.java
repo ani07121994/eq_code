b37aee36a18c822a1779d102aa90f8ccd778f90a
ani07121994 <148828163+ani07121994@users.noreply.github.com>
2023-12-21 16:34:53 +1100

    Delete QantasIHubServices_WCC/QantasIHubServices_WCC directory

--- QantasIHubServices_WCC/QantasIHubServices_WCC/src/qantas/eq/integration/QFEQIFServices.java
+++
@@ -1,9 +0,0 @@
-package qantas.eq.integration;
-
-public class QFEQIFServices {}
-
-/* Location:           D:\Workspace\QantasIHubServices\ImportedClasses
- * Qualified Name:     qantas.eq.integration.QFEQIFServices
- * Java Class Version: 6 (50.0)
- * JD-Core Version:    0.7.1
- */
